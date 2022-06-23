e7a6be59194a1377e51d08a99b7237998dbcead7
Abraham10x <amehabraham811@gmail.com>
2022-06-23 14:47:58 +0100

    Update the header section

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,19 @@ import React from 'react'
 
 function Header(){
     return(
-        <>
-        
-        </>
+        <header>
+            <input type="color" class="color-input" id="color-input" />
+            <select id="color-scheme">
+            <option value="monochrome">Monochrome</option>
+            <option value="monochrome-dark">Monochrome-dark</option>
+            <option value="monochrome-light">Monochrome-light</option>
+            <option value="analogic">Analogic</option>
+            <option value="complement">Complement</option>
+            <option value="analogic-complement">Analogic-complement</option>
+            <option value="triad">Triad</option>
+            </select>
+            <button id="btn">Get color scheme</button>
+      </header>
     )
 }
 
