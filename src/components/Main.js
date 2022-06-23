c1972bc33507f87a5c5ea0619ed3c29cc16f0070
Abraham10x <amehabraham811@gmail.com>
2022-06-23 14:56:42 +0100

    update the Main section

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,8 +2,28 @@ import React from "react";
 
 function Main(){
     return(
-        <>
-        </>
+       <main>
+        <div class="color-bar">
+          <div class="color-one color" id="color-one"></div>
+          <h3 id="color-code-one">#F55A5A</h3>
+        </div>
+        <div class="color-bar">
+          <div class="color-two color" id="color-two"></div>
+          <h3 id="color-code-two">#2B283A</h3>
+        </div>
+        <div class="color-bar">
+          <div class="color-three color" id="color-three"></div>
+          <h3 id="color-code-three">#FBF3AB</h3>
+        </div>
+        <div class="color-bar">
+          <div class="color-four color" id="color-four"></div>
+          <h3 id="color-code-four">#AAD1B6</h3>
+        </div>
+        <div class="color-bar">
+          <div class="color-five color" id="color-five"></div>
+          <h3 id="color-code-five">#D5D4D8</h3>
+        </div>
+      </main>
     )
 }
 
