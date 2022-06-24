17442978c39a1a091a954587726ec8043f274e4b
Abraham10x <amehabraham811@gmail.com>
2022-06-24 16:45:02 +0100

    set up form data using controlled components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 function Header(){
+
+    const [formData, setFormData] = useState({
+        color: '',
+        colorScheme: ''
+    })
+
+    function handleChange(event){
+        const {name, value} = event.target
+        setFormData(preState => {
+            return{
+                ...preState,
+                [name]: value
+            }
+        })
+    }
+
+    function submitData(){
+        console.log(formData)
+    }
+
     return(
         <header>
-            <input type="color" class="color-input" id="color-input" />
-            <select id="color-scheme">
+            <input type="color" 
+                className="color-input" 
+                id="color-input"
+                value={formData.color}
+                onChange={handleChange}
+                name="color"
+            />
+            <select 
+                id="color-scheme" 
+                onChange={handleChange}
+                value={formData.colorScheme}
+                name="colorScheme"
+            >
             <option value="monochrome">Monochrome</option>
             <option value="monochrome-dark">Monochrome-dark</option>
             <option value="monochrome-light">Monochrome-light</option>
@@ -13,7 +44,7 @@ function Header(){
             <option value="analogic-complement">Analogic-complement</option>
             <option value="triad">Triad</option>
             </select>
-            <button id="btn">Get color scheme</button>
+            <button id="btn" onClick={submitData}>Get color scheme</button>
       </header>
     )
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,24 +3,24 @@ import React from "react";
 function Main(){
     return(
        <main>
-        <div class="color-bar">
-          <div class="color-one color" id="color-one"></div>
+        <div className="color-bar">
+          <div className="color-one color" id="color-one"></div>
           <h3 id="color-code-one">#F55A5A</h3>
         </div>
         <div class="color-bar">
-          <div class="color-two color" id="color-two"></div>
+          <div className="color-two color" id="color-two"></div>
           <h3 id="color-code-two">#2B283A</h3>
         </div>
-        <div class="color-bar">
-          <div class="color-three color" id="color-three"></div>
+        <div className="color-bar">
+          <div className="color-three color" id="color-three"></div>
           <h3 id="color-code-three">#FBF3AB</h3>
         </div>
-        <div class="color-bar">
-          <div class="color-four color" id="color-four"></div>
+        <div className="color-bar">
+          <div className="color-four color" id="color-four"></div>
           <h3 id="color-code-four">#AAD1B6</h3>
         </div>
-        <div class="color-bar">
-          <div class="color-five color" id="color-five"></div>
+        <div className="color-bar">
+          <div className="color-five color" id="color-five"></div>
           <h3 id="color-code-five">#D5D4D8</h3>
         </div>
       </main>
